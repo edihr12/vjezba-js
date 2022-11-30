bf316ed19eb0d9e054843cb185c4a5a8dd2b4615
edihr12 <45558723+edihr12@users.noreply.github.com>
2022-11-30 14:22:43 +0100

    evo domaći 5

---
+++ numbers.js
@@ -0,0 +1,20 @@
+var mojeGodine = 41;
+console.log("Moje pseće godine su: ", mojeGodine / 7);
+
+var mojeIme = "Edi";
+var mojePrezime = "Čelar";
+var mojePunoIme = mojeIme + " " + mojePrezime;
+console.log(mojePunoIme);
+
+var prviBroj = Math.random() * 20;
+var drugiBroj = Math.random() * 20;
+console.log("Ostatak pri djeljenu dva random broja je:", prviBroj % drugiBroj);
+
+var brojPolaznika = 22;
+console.log("Trenutni broj polaznika je:", brojPolaznika);
+var brojPolaznika = brojPolaznika + 2;
+console.log("Trenutni broj polaznika je:", brojPolaznika);
+var brojPolaznika = brojPolaznika - 3;
+console.log("Trenutni broj polaznika je:", brojPolaznika);
+var brojPolaznika = brojPolaznika + 5;
+console.log("Trenutni broj polaznika je:", brojPolaznika);
