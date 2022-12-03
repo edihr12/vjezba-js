237dc003b74788ad6a85a1d477e36206c0630c82
edihr12 <45558723+edihr12@users.noreply.github.com>
2022-12-03 23:22:20 +0100

    napravija sam zadnji domaći ostalo mi je još 2. i 3 zadatak. To ću sutra.

--- logicki.js
+++ logicki.js
@@ -55,3 +55,58 @@ Ako je duljina stringa manja od 5. ispisi: Krace od 5!
 Ako je string prazan ispisi: Prazan string!
 Ako je duljina stringa 5 ispisi te znakove
 */
+
+var myString = 'Otorinolaringologija';
+var cutString = myString.substring(4, 9);
+
+if (cutString.length < 5) {
+  console.log('Krace od 5!');
+} else if (cutString.length === 0) {
+  console.log('Prazan string!');
+} else if (cutString.length === 5) {
+  console.log(cutString);
+}
+
+// Zadatak 1.
+// Napisi program koji ovisno o varijabli time mogucih vrijednosti ZORA, JUTRO, POSLIJEPODNE, POPODNE, PRIJEPODNE, PREDVECER, NOC ispisuje Dobro jutro, Dobar dan ili Dobra vecer. Unos time varijable neka bude preko konzole, te ako korisnik nije unio ni jednu od mogucnosti neka ispis bude: Koje je to doba dana? Program napisi uz pomoc if-a i switch case-a.
+
+var time = process.argv[2];
+
+if (time === 'ZORA' || time === 'JUTRO') {
+  console.log('Dobro Jutro!');
+} else if (
+  time === 'PRIJEPODNE' ||
+  time === 'POSLIJEPODNE' ||
+  time === 'POPODNE'
+) {
+  console.log('Dobar dan!');
+} else if (time === 'PREDVECER' || time === 'NOC') {
+  console.log('Dobra večer!');
+} else {
+  console.log('Koje je to doba dana?');
+}
+
+switch (time) {
+  case 'ZORA':
+  case 'JUTRO':
+    console.log('Dobro Jutro!');
+    break;
+  case 'PRIJEPODNE':
+  case 'POSLIJEPODNE':
+  case 'POPODNE':
+    console.log('Dobar dan!');
+    break;
+  case 'PREDVECER':
+  case 'NOC':
+    console.log('Dobra večer!');
+    break;
+  default:
+    console.log('Koje je to doba dana?');
+}
+
+// Zadatak 2.
+// Napisi program koji omogucava unos proizvoljnog broja godina preko konzole, te napisi ternarni izraz koji bira vrijednosti MALOLJETAN i PUNOLJETAN. Potom za MALOLJETAN ispisi "Korisnik je maloljentan", a za punoljetan "Korisnik je punoljetan". Ako korisnik nije unio broj napisi "Pogresan unos!".
+
+// Zadatak 3.
+// Napisi program u kojem korisnik unosi broj preko konzole. Ako korisnik nije unio broj, program racuna da je korisnik unio broj 7. Program zatim ispisuje "Tvoj sretni broj je ___" i potom preko Math.random funkcije izvlaci cijeli broj od 0 do 9. Ukoliko je izvuceni broj identican korisnikovom sretnom broju ispisi "Jackpot!", a u suprotnom "Vise srece drugi put!".
+// Napomena: Ukoliko ne znate koristiti Math.random pokusajte guglati kako se koristi.
